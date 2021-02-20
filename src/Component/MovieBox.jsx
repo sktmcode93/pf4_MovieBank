8f801581b13f96301931ddaae020119ed3069ba2
sktmcode93 <sktmcode93@gmail.com>
2021-02-20 19:14:04 +0900

    update

--- src/Component/MovieBox.jsx
+++ src/Component/MovieBox.jsx
@@ -9,11 +9,11 @@ const MovieBox = (props) => {
         <div className="moviebox">
             <MovieHeader selectList={props.selectList}/>
             <div className="container">
-                <Pages paragraph={props.paragraph} selectPage={props.selectPage}/>
+                {/* <Pages paragraph={props.paragraph} selectPage={props.selectPage}/> */}
                 <div className="movie-main">
                     {drawList(props.movieList, props.selectMovie)}
                 </div>
-                <Pages paragraph={props.paragraph} selectPage={props.selectPage}/>
+                {/* <Pages paragraph={props.paragraph} selectPage={props.selectPage}/> */}
             </div>
         </div>
     )
